a4b2264ce9d75761f158d130c56cb35386fe3f65
EdeCorrea <edecilsc@gmail.com>
2022-05-09 20:47:17 -0300

    Recarrega página do post ao salvar comentários

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from enum import auto
 from django.shortcuts import render
 from blog.models import Post
 from blog.models import Comentario
@@ -11,19 +12,25 @@ def home(request):
 
 def post(request, pk):
     post = Post.objects.get(pk=pk)
-    dados = {"post": post}
+    comentarios_deste_post = Comentario.objects.filter(post=pk)
+    dados = {"post": post,
+             "comentarios": comentarios_deste_post
+            }
     return render(request, 'post.html', dados)
 
 
 def criar_comentario(request, pk):
-    if request.method == "GET":
-        return render(request, 'cria-comentario.html')
-
-    elif request.method == "POST":
+    if request.method == "POST":
         post = Post.objects.get(pk=pk)
-        comentario = request.POST.get('comentario')      
+        comentario = request.POST.get('comentario')
+        if request.user.is_authenticated:
+            autor = request.user
+        else:
+            autor = None
+             
         Comentario.objects.create(post=post,
-                                comentario=comentario)
-        return render(request, 'cria-comentario.html')
+                                comentario=comentario,
+                                autor=autor)
+    return render(request, 'cria-comentario.html')
 
 
